9e285eebc123153b468443c68ce2376c68574ca4
k3nryu <tinkenryu@gmail.com>
2022-07-23 10:49:45 +0900

    update

--- install.sh
+++ install.sh
@@ -4,4 +4,9 @@ brew install golang
 brew install hugo
 
 cd rh-aquatic.com
+
+# install theme
 git clone https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke
+
+# pull page
+git clone git@github.com:kennryuu/kennryuu.github.io.git
